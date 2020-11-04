645dd79a61d6c8810d48fb561b0a95d6404de3cc
m7kvqbe1 <m7kvqbe1@royalnavy.io>
2020-11-04 11:00:29 +0000

    Add minimal repro for DatePicker state bug
    https://github.com/Royal-Navy/design-system/issues/1587

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {
+  DatePicker,
+  DATEPICKER_PLACEMENT,
+} from '@royalnavy/react-component-library'
 
 import { Main } from '../../layouts'
 
-export const Home = () => (
-  <Main>
-    <h1>Hello, World!</h1>
-  </Main>
-)
+export const Home = () => {
+  const [startDate, setStartDate] = useState(new Date(2018, 0, 11))
+
+  function handleOnChange(e: any) {
+    console.log(e)
+    setStartDate(e.startDate)
+  }
+
+  return (
+    <Main>
+      <pre>{startDate.toString()}</pre>
+
+      <DatePicker
+        startDate={startDate}
+        onChange={handleOnChange}
+        placement={DATEPICKER_PLACEMENT.BELOW}
+        isOpen
+      />
+    </Main>
+  )
+}
